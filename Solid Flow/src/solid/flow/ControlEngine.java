236bf286dfc1b887946009185809b18774707e6c
Pelle307 <pellecarlsen@Pelle-Carlsens-MacBook-Pro.local>
2014-11-19 09:49:18 +0100

    its all done.
    vi har fået randomquestion til at fungere

--- Solid Flow/src/solid/flow/ControlEngine.java
+++ Solid Flow/src/solid/flow/ControlEngine.java
@@ -15,11 +15,11 @@ import java.util.Random;
 public class ControlEngine implements WordPairControlInterface {
 
     private ArrayList<WordPair> word;
-    int getQuestion;
+    boolean gotQuestion;
 
     public ControlEngine() {
         word = new ArrayList<WordPair>();
-        getQuestion = 0;
+        gotQuestion = true;
     }
 
     @Override
@@ -35,41 +35,39 @@ public class ControlEngine implements WordPairControlInterface {
 
     @Override
     public String getRandomQuestion() {
+        gotQuestion = true;
         Random random = new Random();
-        int randomNumber = random.nextInt(100) + 1;
+        int randomNumber = 0;
         String question = null;
         int randomQuestion = 0;
-        do {
-            int isAll3 = 0;
+        while (gotQuestion) {
+            randomNumber = random.nextInt(100) + 1;
             randomQuestion = random.nextInt(word.size());
-            if (randomNumber <= 80) {
-                if (word.get(randomQuestion).getScore() == 1) {
-                    question = word.get(randomQuestion).getQuestion() + "";
-                    getQuestion = 0;
-                }
-            } else if (randomNumber >= 81 && randomNumber <= 94) {
-                if (word.get(randomQuestion).getScore() == 2) {
-                    question = word.get(randomQuestion).getQuestion() + "";
-                    getQuestion = 0;
-
-                }
-            } else if (randomNumber >= 95) {
-                if (word.get(randomQuestion).getScore() == 3) {
-                    question = word.get(randomQuestion).getQuestion() + "";
-                    getQuestion = 0;
-                }
+            System.out.println(randomNumber);
+            System.out.println(randomQuestion);
+            if (randomNumber <= 80 && (word.get(randomQuestion).getScore() == 1)) {
+                gotQuestion = false;
             }
-            for (int i = 0; i < word.size(); i++) {
-                if (word.get(i).getScore() == 3) {
-                    isAll3 += 1;
-                }
-                if (isAll3 == word.size()) {
-                    for (int j = 0; j < word.size(); j++) {
-                        word.get(j).setScore(1);
-                    }
-                }
+            if (randomNumber >= 81 && randomNumber <= 94 && (word.get(randomQuestion).getScore() == 2)) {
+                gotQuestion = false;
             }
-        } while (getQuestion == 1);
+            if (randomNumber >= 95 && (word.get(randomQuestion).getScore() == 3)) {
+                gotQuestion = false;
+            }
+        }
+        question = word.get(randomQuestion).getQuestion() + "";
+        
+            int isAll3 = 0;
+        for (WordPair word1 : word) {
+            if (word1.getScore() == 3) {
+                isAll3 += 1;
+            }
+        }
+        if (isAll3 == word.size()) {
+            for (WordPair word1 : word) {
+                word1.setScore(1);
+            }
+        }
         return question;
     }
 
